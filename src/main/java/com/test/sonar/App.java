014384855f698ff8c99484f15e759a0b22bc29ac
parvez <parvezmisarwala@gmail.com>
2018-05-18 11:09:44 +0530

    initial commit

---
+++ src/main/java/com/test/sonar/App.java
@@ -0,0 +1,24 @@
+package com.test.sonar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+class A{
+    	private  String TEST = "";
+    	A(){}
+    	public int dummy(int number) {
+    		number = temp;
+    		if (number==10) {
+    			System.println("Ten");
+    		}
+    	}
+    	
+    }
+}
